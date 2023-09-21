03f6f84fc8e2313d74d4a3b232255a60669d4905
mindofthesky <anjel9494@gmail.com>
2023-09-21 22:53:05 +0900

    09-21 study update

---
+++ 다시 시작하는 C#/code/DelegateBasicExample/DelegateBasicExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBasicExample
+{
+    delegate void LogDel(string text);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //델리게이트는 
+            Log log = new Log();
+            //LogDel logdel = new LogDel(log.LogTextFile);
+
+            LogDel LogTextToScreen, LogTextFile;
+            LogTextToScreen = new LogDel(log.LogTextToScreen);
+            LogTextFile = new LogDel(log.LogTextFile);
+
+            LogDel mulitiLogDel = LogTextToScreen + LogTextFile;
+            Console.WriteLine("이름을 입력해주세요: ");
+            var name = Console.ReadLine();
+
+            mulitiLogDel(name);
+            //logdel(name);
+            LogText(LogTextFile, name);
+            Console.ReadKey();
+
+        }
+        static void LogText(LogDel logDel, string text)
+        {
+          //  logDel(text);
+        }
+
+
+        
+    }
+        public class Log
+        {
+
+            public void LogTextToScreen(string text)
+            {
+                Console.WriteLine($"{DateTime.Now}: {text}");
+            }
+            public void LogTextFile(string text)
+            {
+                    using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), true))
+                    {
+                        sw.WriteLine($"{DateTime.Now}: {text}");
+                    }
+            }
+        }
+}
