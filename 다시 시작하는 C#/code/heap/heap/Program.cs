f12860e397f353bbaf3521f0ccb67608ce676ae6
mindofthesky <anjel9494@gmail.com>
2023-09-28 23:01:41 +0900

    09-28 study update

---
+++ 다시 시작하는 C#/code/heap/heap/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heap
+{
+    public class heap
+    {
+
+        //version heap 1.0 s
+        private List<int> list = new List<int>(); 
+
+        
+
+        public void Add(int x)
+        {
+            list.Add(x);
+
+            int i = list.Count - 1;
+
+            while(i > 0)
+            {
+                int parent = (i - 1) / 2;
+                if (list[parent] < list[i])
+                {
+                    swap(parent, i);
+                    i = parent;
+                }
+                else
+                    break;
+                
+            }
+        }
+        public int remove()
+        {
+
+            int root = list[0];
+
+            list[0] = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+
+            int i = 0;
+            int last = list.Count - 1;
+            while( i< last)
+            {
+                int child = i * 2 + 1;
+
+                if (child < last && list[child] < list[child + 1])
+                    child = child + 1;
+                if (child > last || list[i] > list[child])
+                    break;
+
+                swap(i, child);
+                i = child;
+            }
+
+            return 0;
+        }
+
+        public void swap(int x, int y) 
+        {
+            int k = list[x];
+            list[x] = list[y];
+            list[y] = k;
+
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            heap heapa = new heap();
+            
+        }
+    }
+}
