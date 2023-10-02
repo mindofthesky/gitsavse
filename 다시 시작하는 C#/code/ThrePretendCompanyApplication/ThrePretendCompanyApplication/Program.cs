fc871c92f10a50a2ac9e5c3c075a165bb8615e60
mindofthesky <anjel9494@gmail.com>
2023-10-02 22:25:38 +0900

    10-02 study update

---
+++ 다시 시작하는 C#/code/ThrePretendCompanyApplication/ThrePretendCompanyApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThrePretendCompanyApplication.TCPData;
+
+namespace ThrePretendCompanyApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = Data.GetEmployees();
+            List<Department> departmentList = Data.GetDepartments();
+
+            var resultList = from emp in employeeList
+                             join dept in departmentList
+                             on emp.DepartmentId equals dept.Id
+                             
+                             select new
+                             {
+                                 FirstName = emp.FirstName,
+                                 LastName = emp.LastName,
+                                 AnnualSalary = emp.AnnualSalary,
+                                 Manager = emp.IsManager,
+                                 Department = dept.LongName
+                             };
+
+            foreach (var employee in resultList)
+            {
+                Console.WriteLine($"First Name: {employee.FirstName}");
+                Console.WriteLine($"Last Name: {employee.LastName}");
+                Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+                Console.WriteLine($"Manager: {employee.Manager}");
+                Console.WriteLine($"Department: {employee.Department}");
+                Console.WriteLine();
+            }
+
+            var averageAnnualSalary = resultList.Average(a => a.AnnualSalary);
+            var highestAnnualSalary = resultList.Max(a => a.AnnualSalary);
+            var lowestAnnualSalary = resultList.Min(a => a.AnnualSalary);
+
+            Console.WriteLine($"Average Annual Salary: {averageAnnualSalary}");
+            Console.WriteLine($"Highest Annual Salary: {highestAnnualSalary}");
+            Console.WriteLine($"Lowest Annual Salary: {lowestAnnualSalary}");
+
+            Console.ReadKey();
+        }
+    }
+}
